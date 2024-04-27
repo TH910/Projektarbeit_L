5ca59eaedef8d1a1b3771c6edb533bf268f4feba
TH910 <timo-hinderberger@web.de>
2024-04-27 17:02:43 +0200

    Initial commit

--- Projektarbeit_QT_L/helpfunctions_c.h
+++ Projektarbeit_QT_L/helpfunctions_c.h
@@ -1,6 +1,7 @@
 #ifndef HELPFUNCTIONS_C_H
 #define HELPFUNCTIONS_C_H
 
+#include"messpunkt.h"
 extern "C" {
 float **aufbau_matrix(int zeile, int spalte);
 void abbau_matrix(float **matrix,int zeile);
